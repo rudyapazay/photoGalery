1d902a5f4af1cce4d7dfb1b99f2100a3709159dc
rudyapazay <rudyapazay@gmail.com>
2019-03-01 13:50:49 -0500

    Enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'client-albums';
   author = 'Rudy Apaza yanqui';
-  direction = 'Urb los Lirios SN Mz-E Lt-6';
-  email = 'rudyapazay@gmail.com';
-  blog = 'probando asd';
 }
+ 
